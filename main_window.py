2a9af4ff00ef2ed457b07637907f8c9955d60113
przmkp <przemyslawpak@gmail.com>
2021-10-31 17:43:01 +0100

    Porzadki w klasie MainWindow, dodanie szablonu akcji Open w menu File

--- main_window.py
+++ main_window.py
@@ -1,3 +1,4 @@
+from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QComboBox, QHBoxLayout, QLabel, QMainWindow, QSlider, QStyleFactory, QToolBar, QVBoxLayout, QWidget
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl, Qt
@@ -5,25 +6,26 @@ from gui_elements import *
 class MainWindow(QMainWindow):
     def __init__(self, file_path):
         super(MainWindow, self).__init__()
-        self.setWindowTitle("PZSP2")
-        self.setStyleSheet('MainWindow {background-color:#7d7d7d}')
-        self.setFixedHeight(720)
-        self.setFixedWidth(1280)
+        self.set_defaults()
 
         self.setup_menubar()
         self.setup_webview(file_path)
         self.setup_control_panel()
 
-        main_layout = QHBoxLayout()
-        main_layout.addWidget(self.control_panel)
-        main_layout.addWidget(self.webview)
-        
-        self.central_widget = QWidget()
-        self.central_widget.setLayout(main_layout)
-        self.setCentralWidget(self.central_widget)
+        self.setup_layout()
+
+    def set_defaults(self):
+        self.setWindowTitle("PZSP2")
+        self.setStyleSheet('MainWindow {background-color:#7d7d7d}')
+        self.setFixedHeight(720)
+        self.setFixedWidth(1280)
 
     def setup_menubar(self):
-        self.setMenuBar(MyMenuBar())
+        menu = MyMenuBar()
+        file_open_action = QAction(text='Open', parent=self)
+        file_open_action.triggered.connect(self.file_open)
+        menu.file_menu.addAction(file_open_action)
+        self.setMenuBar(menu)
 
     def setup_webview(self, file_path):
         self.webview = QWebEngineView()
@@ -40,5 +42,17 @@ class MainWindow(QMainWindow):
         control_panel_layout.addWidget(QComboBox())
 
         self.control_panel.setLayout(control_panel_layout)
+
+    def setup_layout(self):
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(self.control_panel)
+        main_layout.addWidget(self.webview)
+        
+        self.central_widget = QWidget()
+        self.central_widget.setLayout(main_layout)
+        self.setCentralWidget(self.central_widget)
+
+    def file_open(self):
+        print('File opened')
 
 
