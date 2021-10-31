2c1fae760f57dcbcd8c7febac6f2ddd5c3ec4303
przmkp <przemyslawpak@gmail.com>
2021-10-31 18:17:26 +0100

    Wyodrebnienie klas MyWebView i ControlPanel

---
+++ gui_elements.py
@@ -0,0 +1,33 @@
+from PySide6.QtCore import QUrl, Qt
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import QComboBox, QMenuBar, QSlider, QTextEdit, QVBoxLayout, QWidget
+
+
+class MyMenuBar(QMenuBar):
+    def __init__(self):
+        super().__init__()
+        self.file_menu = self.addMenu('&File')
+        self.edit_menu = self.addMenu('&Edit')
+        self.selection_menu = self.addMenu('&Selection')
+        self.view_menu = self.addMenu('&View')
+        self.setStyleSheet('QMenuBar {background-color:#515151; color: #f0f0f0;}')
+
+
+class MyWebView(QWebEngineView):
+    def __init__(self, file_path):
+        super().__init__()
+        self.setFixedWidth(600)
+        self.load(QUrl.fromLocalFile(file_path))
+
+
+class ControlPanel(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+        layout.addWidget(QSlider(orientation=Qt.Orientation.Horizontal))
+        layout.addWidget(QSlider(orientation=Qt.Orientation.Horizontal))
+        layout.addWidget(QSlider(orientation=Qt.Orientation.Horizontal))
+        layout.addWidget(QComboBox())
+        layout.addWidget(QComboBox())
+
+        self.setLayout(layout)
